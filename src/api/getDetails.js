caa100970b32219bf91e1da24b24fec307792a1b
necket <nik.sherbakov.01@gmail.com>
2020-04-28 00:51:07 +0300

    alpha 0.1

--- src/api/getDetails.js
+++ src/api/getDetails.js
@@ -3,5 +3,5 @@ import makeRequest from './makeRequest.js';
 const key = 'c268af90';
 
 export default function getFilms(id){
-    return makeRequest(`http://www.omdbapi.com/?i=${id}&apikey=${key}`)
+    return makeRequest(`https://www.omdbapi.com/?i=${id}&apikey=${key}`)
 }
